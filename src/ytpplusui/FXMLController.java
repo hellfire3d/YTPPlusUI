e6b5dd5110c831b6f99eb2a66c74161ce862c1c2
Fuďu Driďák <davidspisovatel@seznam.cz>
2019-07-24 19:17:44 +0200

    Allow all files instead of MP4 only

--- src/ytpplusui/FXMLController.java
+++ src/ytpplusui/FXMLController.java
@@ -171,7 +171,7 @@ public class FXMLController {
     
     @FXML
     void addSource(ActionEvent event) {
-        FileChooser.ExtensionFilter fileFilter = new FileChooser.ExtensionFilter("Video files (*.mp4)", "*.mp4");
+        FileChooser.ExtensionFilter fileFilter = new FileChooser.ExtensionFilter("All files (*.*)", "*.*");
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(fileFilter);
         fileChooser.setTitle("Choose Source");
